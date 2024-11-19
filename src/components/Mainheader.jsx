e2fea1f7cfca123094256ba83240ea774c9172eb
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2024-11-19 21:11:58 +0100

    added shad UI

--- src/components/Mainheader.jsx
+++ src/components/Mainheader.jsx
@@ -5,6 +5,13 @@ import {
   HiOutlinePhone,
 } from "react-icons/hi";
 import Image from "next/image";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function Mainheader() {
   return (
@@ -32,50 +39,57 @@ function Mainheader() {
 
       <div className="w-full mt-4 justify-around items-center gap-10 inline-flex">
         <Image alt="Logo" width={143} height={60} src="/Logo_big.png" />
-        <div className="justify-start items-center gap-2 flex">
-          <div className="px-4 py-2 flex-col justify-start items-start gap-2 inline-flex">
-            <div className="self-stretch justify-start items-center gap-1 inline-flex">
-              <div className="text-[#3c3c3c] text-sm font-semibold   leading-none">
+        <div className="justify-start items-center gap-4 flex">
+          <div className="px-0 py-2 flex-col justify-start items-start gap-2 inline-flex">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="text-[#3c3c3c] text-sm font-semibold w-fit leading-none">
                 About Us
-              </div>
-              <div className="w-5 h-5 relative origin-top-left rotate-90" />
-            </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg-white">
+                <DropdownMenuItem>Who we are</DropdownMenuItem>
+                <DropdownMenuItem>Executive Team</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
-          <div className="px-4 py-2 flex-col justify-start items-start gap-2 inline-flex">
-            <div className="self-stretch justify-start items-center gap-1 inline-flex">
-              <div className="text-[#3c3c3c] text-sm font-semibold   leading-none">
+          <div className="py-2 px-0 justify-start items-center flex">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="text-[#3c3c3c] text-sm font-semibold   leading-none">
                 Membership
-              </div>
-              <div className="w-5 h-5 relative origin-top-left rotate-90" />
-            </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg-white">
+                <DropdownMenuItem>Become a Member</DropdownMenuItem>
+                <DropdownMenuItem>Login as Member</DropdownMenuItem>
+                <DropdownMenuItem>ICAN Website</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
-          <div className="px-4 py-2 rounded-2xl flex-col justify-start items-start gap-2 inline-flex">
-            <div className="self-stretch justify-start items-center gap-1 inline-flex">
-              <div className="text-[#3c3c3c] text-sm font-semibold   leading-none">
-                Donation
-              </div>
-            </div>
+          <div className="px-0 py-2 rounded-2xl flex-col justify-start items-start gap-2 inline-flex">
+            <Link href="/" className="text-[#3c3c3c] text-sm font-semibold">
+              Donation
+            </Link>
           </div>
-          <div className="px-4 py-2 flex-col justify-start items-start gap-2 inline-flex">
-            <div className="self-stretch justify-start items-center gap-1 inline-flex">
-              <div className="text-[#3c3c3c] text-sm font-semibold   leading-none">
+          <div className="px-0 py-2 flex-col justify-start items-start gap-2 inline-flex">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="text-[#3c3c3c] text-sm font-semibold   leading-none">
                 Media & Resources
-              </div>
-              <div className="w-5 h-5 relative origin-top-left rotate-90" />
-            </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg-white">
+                <DropdownMenuItem>Blog</DropdownMenuItem>
+                <DropdownMenuItem>Gallery</DropdownMenuItem>
+                <DropdownMenuItem>Publication and Resources</DropdownMenuItem>
+                <DropdownMenuItem>Jobs</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
           <div className="px-4 py-2 rounded-2xl flex-col justify-start items-start gap-2 inline-flex">
-            <div className="self-stretch justify-start items-center gap-1 inline-flex">
-              <div className="text-[#3c3c3c] text-sm font-semibold   leading-none">
-                Contact Us
-              </div>
-            </div>
+            <Link href="/" className="text-[#3c3c3c] text-sm font-semibold">
+              Contact Us
+            </Link>
           </div>
         </div>
-          <button className="px-3 py-2 bg-[#1a379a] rounded-2xl justify-center items-center gap-2 flex text-white text-sm font-semibold   leading-none">
-            Become a Member
-          </button>
-        
+        <button className="px-3 py-2 bg-[#1a379a] rounded-2xl justify-center items-center gap-2 flex text-white text-sm font-semibold   leading-none">
+          Become a Member
+        </button>
       </div>
     </div>
   );

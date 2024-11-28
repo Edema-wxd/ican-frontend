1b22cdacb772367b8c267495efb14a355433fbe0
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2024-11-28 15:38:31 +0100

    Starting Blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Heroimg from "@/components/Heroimg";
 
 function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div>
+      <Heroimg
+        toggle={false}
+        maintxt={"Blog"}
+        subtxt={
+          "Stay informed with insightful articles and get the latest updates on district activities and ICAN news, all in one place."
+        }
+      >
+        <div className="flex gap-12 flex-row justify-center mt-4">
+          <button className="  text-white rounded-full bg-primary py-3 px-8 w-fit hover:bg-blue-900 ">
+            Become a Member
+          </button>
+          <button className=" outline-1 outline-white text-primary rounded-full bg-blue-100 hover:bg-blue-500 hover:text-white py-3 px-8  w-fit">
+            Learn more
+          </button>
+        </div>
+      </Heroimg>
+    </div>
+  );
 }
 
 export default Blog;

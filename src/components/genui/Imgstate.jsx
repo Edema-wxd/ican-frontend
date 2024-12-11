39b9b964a63426c9854c2a1a6cd4544b3fd72fd9
Woods Francis <woodsedema@gmail.com>
2024-12-11 11:16:25 +0100

    Update Imgstate.jsx

--- src/components/genui/Imgstate.jsx
+++ src/components/genui/Imgstate.jsx
@@ -15,24 +15,6 @@ function Imgstate() {
   );
 }
 
-function Apple() {
-  return (
-    <div className="w-96 h-96 relative rounded-3xl">
-      <div className="w-96 h-96 left-0 top-0 absolute bg-gradient-to-b from-black to-[#1e1e1e] rounded-lg" />
-      <div className="h-48 left-[85px] top-[166px] absolute flex-col justify-start items-center gap-4 inline-flex">
-        <h4 className="text-white text-2xl font-bold font-['Spartan'] leading-loose">
-          ICAN Award & Dinner
-        </h4>
-        <p className="self-stretch text-center text-white text-base font-normal font-['Inter'] leading-normal">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum
-          dolor sit amet, consectetur adipiscing elit
-        </p>
-        <button className="px-8 py-4 bg-[#1a379a] rounded-3xl text-white text-base font-semibold ">
-          View Album
-        </button>
-      </div>
-    </div>
-  );
-}
+
 
 export default Imgstate;

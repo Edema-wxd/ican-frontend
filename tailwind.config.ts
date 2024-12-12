1a6ce52c5f72290c31af2e0135e5a27f9aa9e131
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2024-12-12 10:36:25 +0100

    Fixed the Animate issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import * as animate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -62,7 +63,5 @@ export default {
       },
     },
   },
-  plugins: [
-    //require("tailwindcss-animate")
-  ],
+  plugins: [animate],
 } satisfies Config;

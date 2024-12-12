2a31c9f82e2b47b76970aae2e832091ed767d60a
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2024-12-12 10:33:37 +0100

    Fixed build Issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,44 +1,39 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-import { ReactNode } from 'react';
-
-
-
+// import { ReactNode } from "react";
+import { BlogPost } from "@/types";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
-
 
 /**
  * Shuffles the elements of an array into a random order.
- * 
+ *
  * @param {any[]} array - The input array to be shuffled.
  * @returns {any[]} - A new array with the elements in random order.
  */
 
-export function shuffleArray(array: ReactNode[]) {
-  return array.map((item) => ({
-      item,                // Wrap each element in an object with the original item.
+export function shuffleArray(array: BlogPost[]) {
+  return array
+    .map((item) => ({
+      item, // Wrap each element in an object with the original item.
       sort: Math.random(), // Add a random value for sorting.
     }))
     .sort((a, b) => a.sort - b.sort) // Sort the array based on the random `sort` value.
-    .map(({ item }) => item);        // Extract and return the shuffled items.
+    .map(({ item }) => item); // Extract and return the shuffled items.
 }
-
-
-
 
 /**
  * Handles navigation to a blog post's details page.
- * 
+ *
  * @param {any} router - The router instance, typically from `next/router`.
  * @param {string} id - The unique identifier of the blog post.
  * @returns {void}
  */
 
-export const handleReadMore = (router: ReactNode, id: string) => {
+export const handleReadMore = (router: AppRouterInstance, id: string) => {
   router.push(`/blog/${id}`); // Navigate to the blog post page using the provided ID.
 };

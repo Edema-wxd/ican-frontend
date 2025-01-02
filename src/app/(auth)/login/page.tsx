6835505c7d7360bfea40e62308ee1d5cb2dd6692
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-01-02 12:52:09 +0100

    Stepper component creatoin

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Biodata from "@/components/Biodata";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      Login
+      <Biodata />
+    </div>
+  );
 }
 
 export default Login;

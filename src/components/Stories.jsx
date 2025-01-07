a7a663c0eccce5e8ca41faf3bf44f9d1fd252cd9
Woods Francis <woodsedema@gmail.com>
2025-01-07 07:15:31 +0100

    Update Stories.jsx

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -14,7 +14,7 @@ function Stories() {
     <div className="p-4 sm:p-20 lg:p-40 bg-white flex flex-col  gap-6 justify-between items-center w-full h-fit">
       <div className=" flex flex-col sm:flex-row justify-between items-start sm:items-center w-full  ">
         <h1 className="sm:text-3xl text-2xl font-bold text-blue-900">
-          Explore Our Shared Success Stories
+          Latest Blog Posts
         </h1>
         <Link
           className=" border-b border-solid text-base whitespace-nowrap w-fit border-black "

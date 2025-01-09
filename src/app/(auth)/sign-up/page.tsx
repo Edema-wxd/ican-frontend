e197505025d62bd62725cd69f235c67cb6414c42
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-01-09 15:49:45 +0100

    Form validation for signup

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -7,10 +7,10 @@ import Link from "next/link";
 import {
   FaUser,
   FaEnvelope,
+  FaRegCircle,
   FaLock,
   FaEye,
   FaEyeSlash,
-  FaCircle,
   FaRegCheckCircle,
 } from "react-icons/fa";
 import Toast from "@/components/genui/Toast";
@@ -18,6 +18,7 @@ import Toast from "@/components/genui/Toast";
 function Signup() {
   const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [complete, setComplete] = useState(true);
   const [formData, setFormData] = useState({
     firstName: "",
     surname: "",
@@ -27,6 +28,18 @@ function Signup() {
     consent: false,
   });
   const [showToast, setShowToast] = useState(false);
+
+  const [fname, setFname] = useState(false);
+  const [sname, setSname] = useState(false);
+  const [pvalid, setPvalid] = useState(false);
+  const [cvalid, setCvalid] = useState(false);
+  const [evalid, setEvalid] = useState(false);
+  const [plength, setPlength] = useState(false);
+  const [pupper, setPupper] = useState(false);
+  const [pnumber, setPnumber] = useState(false);
+  const [plower, setPlower] = useState(false);
+  const [consent, setConsent] = useState(false);
+
   const [toastProps, setToastProps] = useState<{
     type: "success" | "error" | "warning" | "info";
     message: string;
@@ -46,10 +59,93 @@ function Signup() {
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
+  const validateFirstName = (firstName: string): string => {
+    const nameRegex = /^[a-zA-Z]+$/;
+    if (firstName.length < 3) {
+      console.log("First name must be at least 3 characters long");
+    }
+    if (!nameRegex.test(firstName)) {
+      console.log("First name must contain only letters.");
+    } else {
+      console.log("First name is valid", firstName);
+      setFname(true);
+    }
+    return "";
+  };
+
+  const validateSurname = (surname: string): string => {
+    const nameRegex = /^[a-zA-Z]+$/;
+    if (surname.length < 3) {
+      return "Surname must be at least 3 characters long.";
+    }
+    if (!nameRegex.test(surname)) {
+      console.log("First name must contain only letters.");
+    } else {
+      console.log("First name is valid", surname);
+      setSname(true);
+    }
+    return "";
+  };
+  const validateEmail = (email: string): string => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      console.log("Invalid email address.", email);
+    } else {
+      console.log("Email is valid", email);
+      setEvalid(true);
+    }
+    return "";
+  };
+  const validatePassword = (password: string): string => {
+    if (password.length < 8) {
+      setPlength(false);
+    } else {
+      setPlength(true);
+    }
+    if (!/[A-Z]/.test(password)) {
+      setPupper(false);
+    } else {
+      setPupper(true);
+    }
+    if (!/[a-z]/.test(password)) {
+      setPlower(false);
+    } else {
+      setPlower(true);
+    }
+    if (!/\d/.test(password) && !/[@$!%*?&]/.test(password)) {
+      setPnumber(false);
+    } else {
+      setPnumber(true);
+    }
+    if (
+      password.length >= 8 &&
+      /[A-Z]/.test(password) &&
+      /[a-z]/.test(password) &&
+      /\d/.test(password) &&
+      /[@$!%*?&]/.test(password)
+    ) {
+      setPvalid(true);
+      console.log("Password is valid");
+    }
+
+    return "";
+  };
+  const validateConfirmPassword = (
+    password: string,
+    cpassword: string
+  ): string => {
+    if (cpassword !== password) {
+      console.log("Passwords do not match");
+    } else {
+      setCvalid(true);
+      console.log("passwords match");
+    }
+    return "";
+  };
 
   const handleChange = (
     e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement 
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
     >
   ) => {
     const { name, value, type } = e.target;
@@ -58,6 +154,33 @@ function Signup() {
       ...formData,
       [name]: type === "checkbox" ? checked : value,
     });
+    if (name === "firstName") {
+      console.log("First Name", value);
+      validateFirstName(value);
+    }
+    if (name === "surname") {
+      console.log("Surname", value);
+      validateSurname(value);
+    }
+    if (name === "email") {
+      console.log("Email", value);
+      validateEmail(value);
+    }
+    if (name === "password") {
+      console.log("Password", value);
+      validatePassword(value);
+    }
+    if (name === "cpassword") {
+      console.log("Confirm password", value);
+      validateConfirmPassword(formData.password, value);
+    }
+    if (name === "consent") {
+      console.log("Consent checked", checked);
+      setConsent(!consent);
+    }
+    if (consent && pvalid && cvalid && evalid && fname && sname) {
+      setComplete(false);
+    }
   };
 
   const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -87,32 +210,12 @@ function Signup() {
       //toast("warning", "All fields are required.");
       return;
     }
-    const passwordRegex =
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if (!passwordRegex.test(password)) {
-      toast(
-        "warning",
-        "Password must be at least 8 characters, contain a letter, a number, and a special character."
-      );
-      return;
-    }
-
-    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
-      toast("warning", "Invalid email address.");
-      return;
-    }
-
-    if (!/^\+?[1-9]\d{1,14}$/.test(phone)) {
-      toast("warning", "Invalid phone number format. (+234)");
-      return;
-    }
-
     console.log("Form submitted", formData);
   };
 
   return (
-    <div className=" m-auto ">
-      <div className="flex flex-col w-[440px] items-center rounded-2xl  bg-white p-8 gap-6 ">
+    <div className=" mx-auto ">
+      <div className="flex flex-col w-96 sm:w-[440px] items-center rounded-2xl  bg-white p-8 gap-6 ">
         <Image src="/Logo_big.png" alt="Logo" width={143} height={60} />
         <div className=" w-fit">
           <h4 className=" text-primary text-center text-3xl font-bold font-mono   ">
@@ -137,6 +240,7 @@ function Signup() {
               id="firstName"
               required
               type="text"
+              onChange={handleChange}
             />
             <p></p>
           </div>
@@ -152,6 +256,7 @@ function Signup() {
               placeholder="Enter your first name"
               name="surname"
               id="surname"
+              onChange={handleChange}
               required
               type="text"
             />
@@ -168,13 +273,14 @@ function Signup() {
               className=" p-3 rounded border border-gray-400  "
               placeholder="Enter your first name"
               name="email"
+              onChange={handleChange}
               id="email"
               required
               type="email"
             />
             <p></p>
           </div>
-          <div className="  w-full flex flex-col">
+          <div className="  w-full flex flex-col ">
             <label
               className=" text-base font-sans font-semibold  "
               htmlFor="password"
@@ -186,8 +292,10 @@ function Signup() {
               <input
                 type={showPassword ? "text" : "password"}
                 id="password"
+                name="password"
                 placeholder="Create a password"
                 className="w-full pl-10 pr-10 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+                onChange={handleChange}
                 required
               />
               <button
@@ -198,21 +306,56 @@ function Signup() {
                 {showPassword ? <FaEyeSlash /> : <FaEye />}
               </button>
             </div>
-            <div className="grid grid-cols-2 gap-4">
-              <p>
-                <FaRegCheckCircle className="w-4 h-4" /> One upper case
-                character
-              </p>
-              <p>
-                <FaCircle className="w-4 h-4" /> 8 characters minimum
-              </p>
-              <p>
-                <FaCircle className="w-4 h-4" /> One lower case character
-              </p>
-              <p>
-                <FaCircle className="w-4 h-4" /> One number and one special
-                character
-              </p>
+            <div className="pt-4 grid grid-cols-2 gap-4">
+              <div
+                className={`text-sm w-fit h-fit justify-start ${
+                  pupper ? " text-green-500 " : "text-gray-500"
+                } items-center flex flex-row gap-3`}
+              >
+                {" "}
+                {pupper ? (
+                  <FaRegCheckCircle className="w-6 h-6 m-1" />
+                ) : (
+                  <FaRegCircle className="w-6 h-6 m-1" />
+                )}
+                <p>One upper case character</p>
+              </div>
+              <div
+                className={`text-sm w-fit h-fit justify-start ${
+                  plength ? " text-green-500 " : "text-gray-500"
+                } items-center flex flex-row gap-3`}
+              >
+                {plength ? (
+                  <FaRegCheckCircle className="w-6 h-6 m-1" />
+                ) : (
+                  <FaRegCircle className="w-6 h-6 m-1" />
+                )}
+                <p className="w-fit">8 characters minimum</p>
+              </div>
+              <div
+                className={`text-sm w-fit h-fit justify-start ${
+                  plower ? " text-green-500 " : "text-gray-500"
+                } items-center flex flex-row gap-3`}
+              >
+                {plower ? (
+                  <FaRegCheckCircle className="w-6 h-6 m-1" />
+                ) : (
+                  <FaRegCircle className="w-6 h-6 m-1" />
+                )}
+                <p className="w-fit">One lower case character</p>
+              </div>
+              <div
+                className={`text-sm w-fit h-fit justify-start ${
+                  pnumber ? " text-green-500 " : "text-gray-500"
+                } items-center flex flex-row gap-3`}
+              >
+                {pnumber ? (
+                  <FaRegCheckCircle className="w-6 h-6 m-1" />
+                ) : (
+                  <FaRegCircle className="w-6 h-6 m-1" />
+                )}
+                <p className="w-fit">One number & one special character</p>
+              </div>
             </div>
           </div>
           <div className="  w-full flex flex-col">
@@ -227,6 +370,8 @@ function Signup() {
               <input
                 type={showPassword ? "text" : "password"}
                 id="cpassword"
+                onChange={handleChange}
+                name="cpassword"
                 placeholder="Create a password"
                 className="w-full pl-10 pr-10 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
                 required
@@ -241,7 +386,7 @@ function Signup() {
             </div>
             <p></p>
           </div>
-          <div className=" flex flex-row justify-between  ">
+          <div className=" flex flex-row justify-between gap-2 items-center ">
             <input
               type="checkbox"
               name="consent"
@@ -249,7 +394,7 @@ function Signup() {
               checked={formData.consent}
               onChange={handleChange}
             />
-            <p>
+            <p className=" text-xs">
               By creating an account, I agree and consent to receive
               communications and updates about ICAN Institute products and
               services. I have reviewed and acknowledged the Privacy Policy
@@ -257,7 +402,8 @@ function Signup() {
             </p>
           </div>
           <button
-            className=" px-8 py-4 bg-primary rounded-full text-white text-base font-semibold "
+            disabled={!complete}
+            className=" px-8 py-4 disabled:bg-slate-500 bg-primary rounded-full text-white text-base font-semibold "
             type="submit"
           >
             Verify

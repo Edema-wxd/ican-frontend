96879e7e87f672551146906eff2e03bed3a9b6ce
owoeyeadenike2020 <112137934+owoeyeadenike2020@users.noreply.github.com>
2025-01-10 00:59:25 +0100

    dashboard layout design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
 import "../globals.css";
+import { Header } from "./ui/Header";
+import { Sidebar } from "./ui/Sidebar";
 
 export default function AuthLayout({
   children,
@@ -8,9 +10,18 @@ export default function AuthLayout({
   return (
     <html lang="en">
       <body>
-        <div className="mt-40">
+        {/* <div className="mt-40">
           <h1>Dashboard layout</h1>
           <div>{children}</div>
+        </div> */}
+        <div className="min-h-screen flex bg-gray-50">
+          <Sidebar />
+          <div className="flex-1 pt-12 bg-gray-50">
+            <Header />
+            <main className="pt-12 p-6">
+              {children}
+            </main>
+          </div>
         </div>
       </body>
     </html>

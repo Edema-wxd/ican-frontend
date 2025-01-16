172b17508e7d8d436fa1eca2aa9b3793cbcb5eb6
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-01-17 00:33:49 +0100

    Updated gallery image

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import BlogCard from "@/components/BlogCard";
+import Studenttile from "./genui/Studenttile";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { handleReadMore } from "@/lib/utils";
@@ -15,21 +16,21 @@ function Stories() {
     <div className="p-4 sm:p-20 lg:p-40 bg-white flex flex-col  gap-6 justify-between items-center w-full h-fit">
       <div className=" flex flex-col sm:flex-row justify-between items-start sm:items-center w-full  ">
         <h1 className="sm:text-3xl text-2xl font-bold text-blue-900">
-          Latest Blog Posts
+          Latest Post
         </h1>
-        <Link
+        {/* <Link
           className=" border-b border-solid text-base whitespace-nowrap w-fit border-black "
           href={"/blog"}
         >
           View all stories
-        </Link>
+        </Link> */}
       </div>
       <section
         className="grid grid-cols-1 lg:grid-cols-2 gap-8"
         aria-label="Blog posts"
       >
         {raw.map((post) => (
-          <BlogCard
+          <Studenttile
             key={post.id}
             post={post}
             onReadMore={() => handleReadMore(router, post.id)}

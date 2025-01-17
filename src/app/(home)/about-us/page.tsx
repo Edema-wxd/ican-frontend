bc13cecb9260f16377908e465321eac3d93c35cc
STEPHEN <harkugbeosaz@gmail.com>
2025-01-17 10:22:25 +0100

    Fix: Updated about page

--- src/app/(home)/about-us/page.tsx
+++ src/app/(home)/about-us/page.tsx
@@ -33,16 +33,6 @@ function Aboutus() {
           </h4>
         </div>
         <div className="flex w-fit flex-col sm:flex-row  gap-12 justify-between">
-          <div className="flex flex-col rounded-xl gap-6 bg-primary max-w-sm p-8 lg:px-8 lg:py-12 text-white">
-            <h6 className=" text-xl font-medium   ">
-              OUR MISSION
-              <hr className="  border opacity-50 w-full border-gray-100   " />
-            </h6>
-            <p className="text-base">
-              To provide an incubation and nurturing centre for professional
-              excellence in young and vibrant accountants
-            </p>
-          </div>
           <div className="flex flex-col rounded-xl gap-6 max-w-sm bg-green-200 text-black p-8">
             <h6 className=" text-xl font-medium  ">
               OUR VISION
@@ -53,19 +43,31 @@ function Aboutus() {
               stars in the accounting profession
             </p>
           </div>
+          <div className="flex flex-col rounded-xl gap-6 bg-primary max-w-sm p-8 lg:px-8 lg:py-12 text-white">
+            <h6 className=" text-xl font-medium   ">
+              OUR MISSION
+              <hr className="  border opacity-50 w-full border-gray-100   " />
+            </h6>
+            <p className="text-base">
+              To provide an incubation and nurturing centre for professional
+              excellence in young and vibrant accountants
+            </p>
+          </div>
+          
         </div>
       </div>
-      <div className=" w-full p-4 sm:py-20 sm:px-40 bg-green-100 sm:bg-blue-100 flex flex-col items-center justify-center gap-16 ">
+      
+      {/* <div className=" w-full p-4 sm:py-20 sm:px-40 flex flex-col items-center justify-center gap-16 bg-[#E0F5E6]">
         <div className="flex flex-col w-fit items-center gap-2">
           <h4 className="text-primary font-mono text-2xl sm:text-5xl font-bold text-center">
             Our Core Values
           </h4>
         </div>
-        <div className="flex flex-col sm:flex-row flex-wrap gap-12 items-center justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 items-center justify-center">
           <Itemcard
             name={"Discipline"}
             info={"Members maintain self-control and adhere to ethical and professional standards"}
-            icon={<MdBrightness5 className="w-9 h-9 fill-primary" />}
+            icon={<MdBrightness5 className="w-8 h-8 fill-primary" />}
             type="col"
             size=" "
             bg="bg-[#dce2f9]"
@@ -101,9 +103,55 @@ function Aboutus() {
             bg="bg-[#dce2f9]"
           />
         </div>
+      </div> */}
+      
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 p-24 sm:p-40 bg-[#d9f5e3] rounded-lg">
+        <h4 className="text-2xl font-bold text-center col-span-1 sm:col-span-2 text-primary font-mono text-2xl sm:text-5xl">
+          Our Core Values
+        </h4>
+
+      <div className="bg-white  rounded-lg shadow-md p-6 border border-dotted border-[#dce2f9] flex flex-col items-start">
+        <div className="mb-4 bg-[#dce2f9] rounded-full p-2">
+          <MdBrightness5 className="w-8 h-8 fill-primary" />
+        </div>
+        <h3 className="font-semibold text-xl mb-2">Discipline</h3>
+        <p className="text-base text-gray-600">
+          Members maintain self-control and adhere to ethical and professional standards
+        </p>
       </div>
+
+      <div className="bg-white  rounded-lg shadow-md p-6 border border-dotted border-[#dce2f9] flex flex-col items-start">
+        <div className="mb-4 bg-[#dce2f9] rounded-full p-2">
+          <MdOutlineLightbulb className="w-8 h-8 fill-primary" />
+        </div>
+        <h3 className="font-semibold text-xl mb-2">Aspiration</h3>
+        <p className="text-base text-gray-600">
+          Members have the mindset of growth in career, business and family
+        </p>
+      </div>
+
+      <div className="bg-white  rounded-lg shadow-md p-6 border border-dotted border-[#dce2f9] flex flex-col items-start">
+        <div className="mb-4 bg-[#dce2f9] rounded-full p-2">
+          <MdFamilyRestroom className="w-8 h-8 fill-primary" />
+        </div>
+        <h3 className="font-semibold text-xl mb-2">Agility</h3>
+        <p className="text-base text-gray-600">
+          Members experiment, learn from failures and iterate on successes quickly and efficiently
+        </p>
+      </div>
+
+      <div className="bg-white  rounded-lg shadow-md p-6 border border-dotted border-[#dce2f9] flex flex-col items-start">
+        <div className="mb-4 bg-[#dce2f9] rounded-full p-2">
+          <MdFlipCameraAndroid className="w-8 h-8 fill-primary" />
+        </div>
+        <h3 className="font-semibold text-xl mb-2">Synergy</h3>
+        <p className="text-base text-gray-600">
+          Members collaborate and unite for collective success.
+        </p>
+      </div>
+    </div>
       <Aims />
-      <Meminfo toggle={true} />
+      {/* <Meminfo toggle={true} /> */}
     </div>
   );
 }

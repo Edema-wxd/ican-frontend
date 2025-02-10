46e5e1e950e3b4b1e652d9f2145ebf0fc61ea102
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-02-10 14:53:42 +0100

    Updated Content dashboard

--- src/components/admincomps/createcontent/NewContent.tsx
+++ src/components/admincomps/createcontent/NewContent.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function NewContent() {
-  return <div>NewContent</div>;
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="flex flex-col p-4 rounded-xl gap-4 bg-white">
+        <div>
+          <h3>Add New Content</h3>
+          <p></p>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default NewContent;

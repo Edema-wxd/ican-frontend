4f28e79e529b5517e10e0474b95b1c5f51e10da7
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-02-17 12:43:40 +0100

    removal of conponent

--- src/components/admincomps/createcontent/specialcomps/Blogtxtbox.tsx
+++ src/components/admincomps/createcontent/specialcomps/Blogtxtbox.tsx
@@ -1,70 +1,7 @@
-import React, { forwardRef, useEffect, useLayoutEffect, useRef } from "react";
-import Quill from "quill";
+import React from "react";
 
-interface EditorProps {
-  readOnly?: boolean;
-  defaultValue?: any;
-  onTextChange?: (...args: any[]) => void;
-  onSelectionChange?: (...args: any[]) => void;
+function Blogtxtbox() {
+  return <div>Blogtxtbox</div>;
 }
-
-const Blogtxtbox = forwardRef<Quill, EditorProps>(
-  ({ readOnly, defaultValue, onTextChange, onSelectionChange }, ref) => {
-    const containerRef = useRef<HTMLDivElement>(null);
-    const defaultValueRef = useRef(defaultValue);
-    const onTextChangeRef = useRef(onTextChange);
-    const onSelectionChangeRef = useRef(onSelectionChange);
-
-    useLayoutEffect(() => {
-      onTextChangeRef.current = onTextChange;
-      onSelectionChangeRef.current = onSelectionChange;
-    });
-
-    useEffect(() => {
-      if (ref && "current" in ref) {
-        ref.current?.enable(!readOnly);
-      }
-    }, [ref, readOnly]);
-
-    useEffect(() => {
-      const container = containerRef.current;
-      if (!container) return;
-
-      const editorContainer = container.appendChild(
-        container.ownerDocument.createElement("div")
-      );
-      const quill = new Quill(editorContainer, {
-        theme: "snow",
-      });
-
-      if (ref && "current" in ref) {
-        ref.current = quill;
-      }
-
-      if (defaultValueRef.current) {
-        quill.setContents(defaultValueRef.current);
-      }
-
-      quill.on('text-change', (...args) => {
-        onTextChangeRef.current?.(...args);
-      });
-
-      quill.on('selection-change', (...args) => {
-        onSelectionChangeRef.current?.(...args);
-      });
-
-      return () => {
-        if (ref && "current" in ref) {
-          ref.current = null;
-        }
-        container.innerHTML = "";
-      };
-    }, [ref]);
-
-    return <div ref={containerRef}></div>;
-  }
-);
-
-Blogtxtbox.displayName = "Blogtxtbox";
 
 export default Blogtxtbox;

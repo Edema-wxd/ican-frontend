b6811996babff9edde0af83312fa8a4fc83e91db
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-02-20 22:17:52 +0100

    Added Cancel event comps

--- src/components/admincomps/event/actions/CancelEvent.tsx
+++ src/components/admincomps/event/actions/CancelEvent.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import {
-  MdSubtitles,
-  MdDeleteOutline,
-} from "react-icons/md";
+import { MdSubtitles, MdDeleteOutline } from "react-icons/md";
 import { HiOutlineTag } from "react-icons/hi";
 
-interface DeleteAdminProps {
+interface CancelEventProps {
   id: string;
-  fullName: string;
-  role: string;
+  eventName: string;
+  date: string;
   onClose: () => void;
 }
 
-function DeleteAdmin({ id, fullName, role, onClose }: DeleteAdminProps) {
+function CancelEvent({ id, eventName, date, onClose }: CancelEventProps) {
   const handleDelete = () => {
-    console.log({ id, fullName, role });
+    console.log({ id, eventName, date });
   };
 
   return (
@@ -26,9 +23,11 @@ function DeleteAdmin({ id, fullName, role, onClose }: DeleteAdminProps) {
           </div>
           <div>
             <div className="flex flex-col gap-2">
-              <h5 className="font-semibold text-xl text-black">Delete Admin</h5>
+              <h5 className="font-semibold text-xl text-black">Cancel Event</h5>
               <p className="text-sm text-neutral-600">
-                Are you sure you want to delete this admin?
+                If you cancel this event, the event will no longer take place
+                and members will be notified via email. Are you sure you want to
+                proceed?{" "}
               </p>
             </div>
             <div className="flex flex-col gap-2 mt-4">
@@ -36,13 +35,13 @@ function DeleteAdmin({ id, fullName, role, onClose }: DeleteAdminProps) {
                 <p className="flex text-neutral-700 font-medium text-base flex-row  items-center gap-2">
                   <MdSubtitles className="w-4 h-4" /> Name:
                 </p>
-                <p className="text-black font-medium text-base ">{fullName}</p>
+                <p className="text-black font-medium text-base ">{eventName}</p>
               </div>
               <div className="flex flex-row items-center gap-4">
                 <p className="flex text-neutral-700 font-medium text-base   items-centerflex-row gap-2">
-                  <HiOutlineTag className="w-4 h-4" /> Role:
+                  <HiOutlineTag className="w-4 h-4" /> Date:
                 </p>
-                <p className="text-black font-medium text-base ">{role}</p>
+                <p className="text-black font-medium text-base ">{date}</p>
               </div>
             </div>
           </div>
@@ -52,7 +51,7 @@ function DeleteAdmin({ id, fullName, role, onClose }: DeleteAdminProps) {
             onClick={handleDelete}
             className="flex items-center w-2/5 text-center justify-center bg-red-600 font-semibold text-base text-white rounded-full py-3 px-4 h-10"
           >
-            Delete
+            Yes, Please
           </button>
           <button
             onClick={onClose}
@@ -66,4 +65,4 @@ function DeleteAdmin({ id, fullName, role, onClose }: DeleteAdminProps) {
   );
 }
 
-export default DeleteAdmin;
+export default CancelEvent;
--- src/components/admincomps/event/actions/CellActions.tsx
+++ src/components/admincomps/event/actions/CellActions.tsx
@@ -19,7 +19,7 @@ import { FaUserXmark, FaUserCheck } from "react-icons/fa6";
 
 import { MoreHorizontal } from "lucide-react";
 import DisableAdmin from "./DisableAdmin";
-import DeleteAdmin from "./DeleteAdmin";
+import CancelEvent from "./CancelEvent";
 import EnableAdmin from "./EnableAdmin";
 
 interface CellProps {
@@ -35,99 +35,72 @@ const ActionsCell: React.FC<CellProps> = ({ row }) => {
     return str.replace(/\b\w/g, (char) => char.toUpperCase());
   };
 
-  if (row.original.role === "member") {
-    return (
-      <>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {row.original.status === "active" ? (
-              <DropdownMenuItem className="flex flex-row items-center">
-                <FaUserXmark className="w-4 h-4" /> Suspend User
-              </DropdownMenuItem>
-            ) : (
-              <DropdownMenuItem className="flex flex-row items-center">
-                <FaUserCheck className="w-4 h-4" /> Reactivate User
-              </DropdownMenuItem>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </>
-    );
-  }
-  if (row.original.role === "admin" || row.original.role === "super admin") {
-    return (
-      <>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem className="flex flex-row items-center">
-              <MdEdit className="w-4 h-4" /> Edit Admin Details
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            {row.original.status === "active" ? (
-              <DropdownMenuItem
-                className="flex flex-row items-center"
-                onClick={() => setShowDisableModal(true)}
-              >
-                <MdOutlineToggleOn className="w-4 h-4" /> Disable Admin
-              </DropdownMenuItem>
-            ) : (
-              <DropdownMenuItem
-                className="flex flex-row items-center"
-                onClick={() => setShowEnableModal(true)}
-              >
-                <MdOutlineToggleOff className="w-4 h-4" /> Enable Admin
-              </DropdownMenuItem>
-            )}
-
-            <DropdownMenuSeparator />
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem className="flex flex-row items-center">
+            <MdEdit className="w-4 h-4" /> Edit Event Details
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          {row.original.status === "active" ? (
             <DropdownMenuItem
-              className="hover:bg-red-600 hover:text-white text-red-600 flex flex-row items-center fill-red-600 "
-              onClick={() => setShowDeleteModal(true)}
+              className="flex flex-row items-center"
+              onClick={() => setShowDisableModal(true)}
             >
-              <MdOutlineDelete className="w-4 h-4" />
-              Delete
+              <MdOutlineToggleOn className="w-4 h-4" /> Disable Admin
             </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        {showDisableModal && (
-          <DisableAdmin
-            id={row.original.id}
-            fullName={row.original.fullName}
-            role={capitalizeWords(row.original.role ?? "")}
-            onClose={() => setShowDisableModal(false)}
-          />
-        )}
-        {showEnableModal && (
-          <EnableAdmin
-            id={row.original.id}
-            fullName={row.original.fullName}
-            role={capitalizeWords(row.original.role ?? "")}
-            onClose={() => setShowDisableModal(false)}
-          />
-        )}
-        {showDeleteModal && (
-          <DeleteAdmin
-            id={row.original.id}
-            fullName={row.original.fullName}
-            role={capitalizeWords(row.original.role ?? "")}
-            onClose={() => setShowDeleteModal(false)}
-          />
-        )}
-      </>
-    );
-  }
+          ) : (
+            <DropdownMenuItem
+              className="flex flex-row items-center"
+              onClick={() => setShowEnableModal(true)}
+            >
+              <MdOutlineToggleOff className="w-4 h-4" /> Enable Admin
+            </DropdownMenuItem>
+          )}
+
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="hover:bg-red-600 hover:text-white text-red-600 flex flex-row items-center fill-red-600 "
+            onClick={() => setShowDeleteModal(true)}
+          >
+            <MdOutlineDelete className="w-4 h-4" />
+            Cancel Event
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {showDisableModal && (
+        <DisableAdmin
+          id={row.original.id}
+          fullName={row.original.fullName}
+          role={capitalizeWords(row.original.role ?? "")}
+          onClose={() => setShowDisableModal(false)}
+        />
+      )}
+      {showEnableModal && (
+        <EnableAdmin
+          id={row.original.id}
+          fullName={row.original.fullName}
+          role={capitalizeWords(row.original.role ?? "")}
+          onClose={() => setShowDisableModal(false)}
+        />
+      )}
+      {showDeleteModal && (
+        <CancelEvent
+          id={row.original.id}
+          eventName={row.original.eventName}
+          date={row.original.date}
+          onClose={() => setShowDeleteModal(false)}
+        />
+      )}
+    </>
+  );
 };
 
 export default ActionsCell;

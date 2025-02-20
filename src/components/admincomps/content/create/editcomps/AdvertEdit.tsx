1e9a550fc9702915481fdc0b16789308d61bebe1
Edema-wxd <woodsedema@gmail.com>
2025-02-20 18:44:35 +0100

    Base Building of all  edit components

--- src/components/admincomps/content/create/editcomps/AdvertEdit.tsx
+++ src/components/admincomps/content/create/editcomps/AdvertEdit.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import InputEle from "@/components/genui/InputEle";
 
 function AdvertEdit() {
-  return <div>AdvertEdit</div>;
-}
+const [advert , setAdvert ] = useState("")
+
+return  <div>
+        <div>
+          <InputEle label="Advert Title" type="text" id="title" onChange={() => {}} />
+          <InputEle label="Advertisement Image" type="images" id="advert_image" onChange={() => {}} />
+      
+         
+        </div>
+        <div className="flex flex-col gap-2">
+          <button className="rounded-full py-2 bg-primary text-white text-base w-full">
+            Publish Advert
+          </button>
+          <button className=" py-2 text-primary border border-primary text-base rounded-full w-full">
+            Save as Draft
+          </button>
+          <button className=" py-1 text-primary text-base rounded-full w-full">
+            Preview
+          </button>
+        </div>
+      </div>;}
 
 export default AdvertEdit;
--- src/components/admincomps/content/create/editcomps/PublicationEdit.tsx
+++ src/components/admincomps/content/create/editcomps/PublicationEdit.tsx
@@ -15,7 +15,7 @@ function PublicationEdit() {
         </div>
         <div className="flex flex-col gap-2">
           <button className="rounded-full py-2 bg-primary text-white text-base w-full">
-            Publish Blog
+            Publish Publication
           </button>
           <button className=" py-2 text-primary border border-primary text-base rounded-full w-full">
             Save as Draft

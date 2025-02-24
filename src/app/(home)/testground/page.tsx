6f228dd2cf9f5098a09dce36edf18b40b58cfa69
Edema-wxd <woodsedema@gmail.com>
2025-02-24 13:38:37 +0100

    Rich text Edit

---
+++ src/app/(home)/testground/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import RichTextEditor from "@/components/genui/RichTextEditor";
+import React from "react";
+
+function TestingPage() {
+  return (
+    <div className="mt-20">
+      TestingPage
+      <div className="m-10">
+        <RichTextEditor />
+      </div>
+    </div>
+  );
+}
+
+export default TestingPage;

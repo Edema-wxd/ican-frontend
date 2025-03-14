4d37a7f152a9ff752821cc5e39da51ac76d46e82
Edema-wxd <woodsedema@gmail.com>
2025-03-14 10:57:10 +0100

    Add Billing details and creation pages, implement BillingTable component for invoice management

---
+++ src/app/(admin)/admin/billing/[id]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+function BillingDetailsPage() {
+  return <div>BillingDetailsPage </div>;
+}
+
+export default BillingDetailsPage;
--- src/app/(admin)/admin/billing/page.tsx
+++ src/app/(admin)/admin/billing/page.tsx
@@ -1,9 +1,39 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
+
+import { BillingTable } from "@/components/admincomps/billing/datatable/BillingTable";
 
 function BillingPage() {
-  return <div>BillingPage</div>;
+  const router = useRouter();
+  return (
+    <div className="rounded-3xl p-6">
+      <div className="flex flex-row mb-6 w-full items-center justify-between">
+        <h2 className="flex flex-col gap-2 text-left items-start font-semibol text-2xl text-black">
+          Billing{" "}
+          <span className="text-base text-gray-600">
+            Bill members and track their invoices here
+          </span>
+        </h2>
+        <button
+          className="bg-primary text-white text-base font-semibold rounded-full w-fit px-3 py-2 whitespace-nowrap h-fit"
+          onClick={() => router.push("/admin/billing/create")}
+        >
+          Create New Bill
+        </button>
+      </div>
+      {/* Tab sections */}
+      <div className="rounded-3xl px-8 py-6 flex flex-col gap-4 border border-neutral-200 bg-white">
+        <h2 className="text-xl w-full font-semibold text-left border-b border-gray-500 ">
+          Notifications
+        </h2>
+        <div>
+          <BillingTable columns={} data={} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BillingPage;
